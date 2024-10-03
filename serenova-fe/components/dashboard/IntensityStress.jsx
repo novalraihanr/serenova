958525b05d24ee19856dd6013e46b171aa8cefb3
hanselabilo <hansel.abilo@gmail.com>
2024-10-03 20:22:22 +0700

    Update Intensity Stress

--- serenova-fe/components/dashboard/IntensityStress.jsx
+++ serenova-fe/components/dashboard/IntensityStress.jsx
@@ -1,16 +1,35 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import DashStressCheck from "./DashStressCheck";
 import IntensityBar from "./IntensityBar";
-import Image from "next/image";
 import TestResult from "./TestResult";
 
 const IntensityStress = () => { 
+    const [stressData, setStressData] = useState(null); 
+
+    useEffect(() => {
+        const fetchStressData = async () => {
+            try {
+                const response = await fetch('/api/stressapi');
+                const data = await response.json();
+
+                if (data.score && data.date) {
+                    setStressData(data); 
+                }
+            } catch (error) {
+                console.error("Error fetching stress data:", error);
+            }
+        };
+
+        fetchStressData(); 
+    }, []);
+
     return (
-        <div className="flex mt-5 gap-x-3 ">
+        <div className="flex flex-col lg:flex-row mt-5 gap-x-3 gap-y-5">
             {/* KIRI */}
-            <div className="flex flex-col w-1/2 h-auto"> 
-                <div className="">
+            <div className="flex flex-col w-full lg:w-1/2 h-auto"> 
+                <div>
                     <h1 className="text-bgButton font-semibold">Daily Activity Intensity</h1>
                 </div>
                 <div className="mt-4 bg-white rounded-lg p-7 flex-1"> 
@@ -23,17 +42,18 @@ const IntensityStress = () => {
                     </p>
                 </div>
             </div>
+            
             {/* KANAN */}
-            <div className="flex flex-col w-1/2 h-auto"> 
-                <div className="">
+            <div className="flex flex-col w-full lg:w-1/2 h-auto"> 
+                <div>
                     <h1 className="text-bgButton font-semibold">Stress Checker</h1> 
                 </div>
-                <div className="mt-4 bg-white rounded-lg flex-1"> 
-                    <TestResult />
+                <div className="mt-4 bg-white rounded-lg flex-1">
+                    {stressData ? <TestResult stressData={stressData} /> : <DashStressCheck />}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default IntensityStress;
